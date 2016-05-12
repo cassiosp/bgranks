8514eaa5cab7e681e62337e784321b5b5a6108a3
Cassio Pereira <cassio.pereira@gmail.com>
2016-05-11 18:54:29 -0700

    AngulasJS install with bower and basic assets pipeline integration with initial controller with hardcoded data.

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,1 @@
+var bgRanks = angular.module('bgRanks', []);
---
+++ app/assets/javascripts/games/gamesCtrl.js
@@ -0,0 +1,10 @@
+bgRanks.controller('GamesCtrl', ['$scope', function($scope){
+  $scope.games = [
+    {'name': 'Trajan',
+     'designer': 'Stefan Feld'},
+    {'name': 'Pandemic Legacy',
+     'designer': 'Rob Daviau, Matt Leacock'},
+    {'name': 'Trickerion: Legends of Illusion',
+     'designer': 'Richard Amann, Viktor Peter'}
+  ];
+}]);
